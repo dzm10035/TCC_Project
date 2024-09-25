0f0acc126df57dc72dfd0290adb1df2562cbb847
dzm10035 <dingzhenman03@gmail.com>
2024-09-25 20:44:08 +0800

    new

--- connection.php
+++ connection.php
@@ -4,7 +4,7 @@
     $password = "";     
     $dbname = "tcc_project"; 
 
-    $conn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
 
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
